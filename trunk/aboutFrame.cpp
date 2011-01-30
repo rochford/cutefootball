2539d9a25bd30d1aa7f0145249d1b520748edf1f
timothy.rochford <timothy.rochford@77807927-c41d-85ec-7979-c5442699f4c2>
2011-01-30 18:50:37 +0000

    code cleanup - states moved to separate files.

--- trunk/aboutFrame.cpp
+++ trunk/aboutFrame.cpp
@@ -1,12 +1,16 @@
 #include "aboutFrame.h"
 #include "ui_aboutFrame.h"
 #include "mainwindow.h"
+#include "compileTimeSettings.h"
 
 aboutFrame::aboutFrame(MWindow *parent) :
     QFrame(parent),
     uiAbout(new Ui::aboutFrame)
 {
     uiAbout->setupUi(this);
+    uiAbout->versionLabel->setText(versionNumber);
+
+
     connect(uiAbout->buttonBox, SIGNAL(accepted()), parent, SLOT(hideAboutFrame()));
     connect(uiAbout->buttonBox, SIGNAL(rejected()), parent, SLOT(hideAboutFrame()));
 }
--- trunk/game.cpp
+++ trunk/game.cpp
@@ -1,184 +1,10 @@
 #include "game.h"
+#include "goalscoredstate.h"
+#include "foulstate.h"
 #include "pitch.h"
 #include "ball.h"
 #include "Player.h"
 #include "screengraphics.h"
-
-FoulState::FoulState(Game *g, Pitch *p)
-    : QState(g),
-      m_game(g),
-      m_pitch(p)
-{
-    m_takePositions = new QState(this);
-    m_allDone = new QFinalState(this);
-    setInitialState(m_takePositions);
-
-    m_timeLineTakePositions = new QTimeLine(1000*3, this);
-    m_timeLineTakePositions->setCurveShape(QTimeLine::LinearCurve);
-    m_timeLineTakePositions->setFrameRange(0, 100);
-
-    m_takePositions->addTransition(m_takePositions, SIGNAL(finished()), m_allDone);
-
-    connect(m_timeLineTakePositions, SIGNAL(frameChanged(int)), this, SLOT(playFrame(int)));
-    connect(m_timeLineTakePositions, SIGNAL(finished()), m_game, SLOT(kickOff()));
-}
-
-void FoulState::onEntry(QEvent * /* event */)
-{
-    qDebug() << "FoulState::onEntry";
-    m_game->stopGameClock();
-    createPlayerAnimationItems(TakePositions);
-    m_timeLineTakePositions->start();
-}
-
-// animate from present player position to another point.
-void FoulState::createPlayerAnimationItems(GameState g)
-{
-    m_playerAnimationItems.clear(); // TODO XXX TIM delete all
-
-    foreach (Player *p, m_pitch->m_players) {
-        QGraphicsItemAnimation* anim = new QGraphicsItemAnimation(this);
-        anim->setItem(p);
-        m_playerAnimationItems.append(anim);
-
-        QPointF tmp;
-        qreal stepX;
-        qreal stepY;
-
-        switch( g )
-        {
-        case TakePositions:
-        {
-            anim->setTimeLine(m_timeLineTakePositions);
-            tmp = p->pos();
-            stepX = ( p->m_startPositionRectF.center().x() - tmp.x()) / 100.0;
-            stepY = ( p->m_startPositionRectF.center().y() - tmp.y()) / 100.0;
-            MWindow::Action a = calculateAction(tmp, p->m_startPositionRectF.center());
-            p->movePlayer(a);
-        }
-            break;
-
-        default:
-            break;
-        }
-
-        for (int i = 0; i < 100; ++i) {
-            anim->setPosAt(i / 100.0, QPointF(tmp.x() + stepX,
-                                              tmp.y() + stepY));
-            tmp.setX(tmp.x() + stepX);
-            tmp.setY(tmp.y() + stepY);
-        }
-    }
-}
-
-void FoulState::playFrame(int frame)
-{
-    qreal f = frame/ 100.00;
-    foreach (QGraphicsItemAnimation *anim, m_playerAnimationItems)
-        anim->item()->setPos(anim->posAt(f));
-    m_pitch->m_scene->update();
-}
-
-GoalScoredState::GoalScoredState(Game *g, Pitch *p)
-    : QState(g),
-      m_game(g),
-      m_pitch(p)
-{
-    m_celebrate = new QState(this);
-    m_returnToPosition = new QState(this);
-    m_allDone = new QFinalState(this);
-    setInitialState(m_celebrate);
-
-    m_timeLineCelebrate = new QTimeLine(1000*3, this);
-    m_timeLineCelebrate->setCurveShape(QTimeLine::LinearCurve);
-    m_timeLineCelebrate->setFrameRange(0, 100);
-
-    m_timeLineReturnStartPositions = new QTimeLine(1000*3, this);
-    m_timeLineReturnStartPositions->setCurveShape(QTimeLine::LinearCurve);
-    m_timeLineReturnStartPositions->setFrameRange(0, 100);
-
-    m_celebrate->addTransition(m_timeLineCelebrate, SIGNAL(finished()), m_returnToPosition);
-    m_returnToPosition->addTransition(m_timeLineReturnStartPositions, SIGNAL(finished()), m_allDone);
-
-    connect(m_timeLineCelebrate, SIGNAL(frameChanged(int)), this, SLOT(playFrame(int)));
-    connect(m_timeLineCelebrate, SIGNAL(finished()), this, SLOT(createTakePositionAnimation()));
-    connect(m_timeLineReturnStartPositions, SIGNAL(frameChanged(int)), this, SLOT(playFrame(int)));
-    connect(m_timeLineReturnStartPositions, SIGNAL(finished()), m_game, SLOT(kickOff()));
-}
-
-void GoalScoredState::createTakePositionAnimation()
-{
-    createPlayerAnimationItems(TakePositions);
-    m_timeLineReturnStartPositions->start();
-}
-
-void GoalScoredState::onEntry(QEvent * /* event */)
-{
-    m_pitch->updateDisplayTime(m_game->remainingTimeInHalfMs());
-    m_pitch->ball()->setVisible(false);
-    m_game->stopGameClock();
-    createPlayerAnimationItems(Celebrate);
-    m_timeLineCelebrate->start();
-}
-
-// animate from present player position to another point.
-void GoalScoredState::createPlayerAnimationItems(GameState g)
-{
-    m_playerAnimationItems.clear(); // TODO XXX TIM delete all
-
-    foreach (Player *p, m_pitch->m_players) {
-        if ( g == Celebrate && !p->team()->scoredLastGoal() )
-            continue;
-
-        QGraphicsItemAnimation* anim = new QGraphicsItemAnimation(this);
-        anim->setItem(p);
-        m_playerAnimationItems.append(anim);
-
-        QPointF tmp;
-        qreal stepX;
-        qreal stepY;
-
-        switch( g )
-        {
-        case TakePositions:
-        {
-            anim->setTimeLine(m_timeLineReturnStartPositions);
-            tmp = p->pos();
-            stepX = ( p->m_startPositionRectF.center().x() - tmp.x()) / 100.0;
-            stepY = ( p->m_startPositionRectF.center().y() - tmp.y()) / 100.0;
-            MWindow::Action a = calculateAction(tmp, p->m_startPositionRectF.center());
-            p->movePlayer(a);
-        }
-            break;
-
-        case Celebrate:
-            anim->setTimeLine(m_timeLineCelebrate);
-            tmp = p->pos();
-            stepX = ( m_pitch->m_footballPitch->rect().center().x() - tmp.x() ) / 100.0;
-            stepY = ( m_pitch->m_footballPitch->rect().center().y() - tmp.y() ) / 100.0;
-            break;
-
-        default:
-            break;
-        }
-
-        for (int i = 0; i < 100; ++i) {
-            anim->setPosAt(i / 100.0, QPointF(tmp.x() + stepX,
-                                              tmp.y() + stepY));
-            tmp.setX(tmp.x() + stepX);
-            tmp.setY(tmp.y() + stepY);
-        }
-    }
-}
-
-void GoalScoredState::playFrame(int frame)
-{
-    qreal f = frame/ 100.00;
-    foreach (QGraphicsItemAnimation *anim, m_playerAnimationItems)
-        anim->item()->setPos(anim->posAt(f));
-    m_pitch->m_scene->update();
-}
-
 
 Game::Game(Pitch* p,
      QString stateName,
@@ -223,6 +49,15 @@ Game::Game(Pitch* p,
     connect(m_1second, SIGNAL(timeout()), this, SLOT(decrementGameTime()));
 
     connect(m_playingState, SIGNAL(entered()), m_pitch, SLOT(gameStarted()));
+}
+
+Game::~Game()
+{
+    m_1second->stop();
+    delete m_1second;
+
+    delete m_timeLineTakePositions;
+    delete m_timeLineLeavePitch;
 }
 
 void Game::setGameLength(int totalGameInMinutes)
